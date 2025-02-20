5fc5d1994614667a5bc0b98aedf0c869e6e26a03
ImtiazAhmed01 <imtiazahm21@gmail.com>
2025-02-20 14:23:00 -0500

    initializing server

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+const { ObjectId } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.khtuk.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        const database = client.db("taskmanagementapp");
+        const usersCollection = database.collection("users");
+        app.post("/users", async (req, res) => {
+            try {
+                const { uid, email, displayName } = req.body;
+
+                if (!uid || !email || !displayName) {
+                    return res.status(400).json({ error: "Invalid user data" });
+                }
+
+                const existingUser = await usersCollection.findOne({ uid });
+
+                if (!existingUser) {
+                    const newUser = { uid, email, displayName, createdAt: new Date() };
+                    await usersCollection.insertOne(newUser);
+                    return res.status(201).json({ message: "User saved", user: newUser });
+                }
+
+                res.status(200).json({ message: "User already exists", user: existingUser });
+            } catch (error) {
+                console.error("Error saving user:", error.message);
+                res.status(500).json({ error: "Internal server error" });
+            }
+        });
+
+
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+    // finally {
+    //     // // Ensures that the client will close when you finish/error
+    //     // await client.close();
+    // }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('SIMPLE CRUD IS RUNNING')
+})
+app.listen(port, () => {
+    console.log(`SIMPLE crud is running on port: ${port}`)
+
+})
